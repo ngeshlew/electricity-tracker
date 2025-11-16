90bc3c55bb622a27ba88dff2f0d639cd7e223b6f
Ngeshlew <ngugilewis@gmail.com>
2025-11-16 14:43:20 +0000

    fix(build): remove unused QuestionMarkCircleIcon import in UserGuide

--- src/components/help/UserGuide.tsx
+++ src/components/help/UserGuide.tsx
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { Button } from '@/components/ui/button';
 import { 
-  QuestionMarkCircleIcon,
   XMarkIcon,
   ChevronRightIcon,
   ChevronDownIcon,
